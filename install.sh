ca1d3387c6895419ae282c5620e5f1e15647b92d
Josh Enders <josh.enders@gmail.com>
2011-10-14 16:29:58 -0700

    improved install script

--- install.sh
+++ install.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
 # Instead of working with dotfiles directly, they've been renamed in git.
-# This script "fixes" that.
+# This script addresses that and moves them in to $HOME.
 
-# circle back and make this more robust
-ln -sfv ${PWD}/bash_profile ~/.bash_profile
-ln -sfv ${PWD}/bash_profile.osx ~/.bash_profile.osx
-ln -sfv ${PWD}/bash_profile.linux ~/.bash_profile.linux
-ln -sfv ${PWD}/gitconfig ~/.gitconfig
-ln -sfv ${PWD}/screenrc ~/.screenrc
-ln -sfv ${PWD}/vim ~/.vim
-ln -sfv ${PWD}/vimrc ~/.vimrc
+echo "WARNING: This operation will remove existing files in $HOME."
+
+while [[ -z "$response" ]]; do
+    read -p "Are you ready to install? [y/n] " response
+    
+    if [[ "$response" =~ [yY] ]]; then
+	printf "\n"
+        break
+    else
+        exit 1
+    fi
+done
+
+dotfiles=(
+    bash_profile
+    bash_profile.linux
+    bash_profile.osx
+    gitconfig
+    htoprc
+    screenrc
+    vim
+    vimrc
+) 
+
+for file in ${dotfiles[@]}; do
+    echo "Working on $file"
+
+    if [[ -e "${HOME}/.${file}" ]]; then
+        rm -rfv "${HOME}/.${file}"
+    fi
+    ln -sfv "${PWD}/${file}" "${HOME}/.${file}"
+done
