a27b1fe5a09c5785acbed3bc8e30a871d7e3e89e
Josh Enders <josh.enders@gmail.com>
2011-10-23 00:22:42 -0700

    some sperging on install.sh

--- install.sh
+++ install.sh
@@ -3,16 +3,18 @@
 # Instead of working with dotfiles directly, they've been renamed in git.
 # This script addresses that and moves them in to $HOME.
 
-echo "WARNING: This operation will remove existing files in $HOME."
+printf "WARNING: This operation will remove existing files in $HOME.\n\n"
 
 while [[ -z "$response" ]]; do
     read -p "Are you ready to install? [y/n] " response
     
-    if [[ "$response" =~ [yY] ]]; then
+    if [[ "$response" =~ ^[yY]$ ]]; then
 	printf "\n"
         break
-    else
+    elif [[ "$response" =~ ^[nN]$ ]]; then
         exit 1
+    else
+        unset response
     fi
 done
 
