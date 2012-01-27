273679b348ad291214c018a619bdb780902ab29f
Josh Enders <jenders@jenders-air.lan>
2012-01-27 00:18:10 -0800

    I decided I like my dotfiiles to be named properly. I can live with [34m.[39;49m[0m/ [34m..[39;49m[0m/ .bash_profile .bash_profile.linux .bash_profile.osx [34m.git[39;49m[0m/ .gitconfig .gitignore .htoprc .my.cnf .screenrc [34m.vim[39;49m[0m/ .vimrc README.md [31minstall.sh[39;49m[0m* irssi_config ssh_config. Touched the install.sh script to handle private files now too. Also wrote a little blurb of markdown in the readme

---
+++ README.md
@@ -0,0 +1,5 @@
+About
+=====
+
+This is my dotfiles repo. The files are versioned in git and I create symlinks 
+in my home directory with the included `install.sh` script.
--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Instead of working with dotfiles directly, they've been renamed in git.
-# This script addresses that and moves them in to $HOME.
+# This script creates symlinks in $HOME for certain files in this directory
 
 printf "WARNING: This operation will remove existing files in $HOME.\n\n"
 
@@ -18,23 +17,45 @@ while [[ -z "$response" ]]; do
     fi
 done
 
+#
+# Public files
+#
+
 dotfiles=(
-    bash_profile
-    bash_profile.linux
-    bash_profile.osx
-    gitconfig
-    htoprc
-    screenrc
-    vim
-    vimrc
-    my.cnf
+    .bash_profile
+    .bash_profile.linux
+    .bash_profile.osx
+    .gitconfig
+    .htoprc
+    .screenrc
+    .vim
+    .vimrc
+    .my.cnf
 ) 
 
 for file in ${dotfiles[@]}; do
     echo "Working on $file"
 
-    if [[ -e "${HOME}/.${file}" ]]; then
+    if [[ -e "${HOME}/.${file}" ]]; then # remove existing files and directories
         rm -rfv "${HOME}/.${file}"
     fi
-    ln -sfv "${PWD}/${file}" "${HOME}/.${file}"
+    ln -sfv "${PWD}/${file}" "${HOME}/${file}" # create symlinks for each file pointing here
 done
+
+#
+# Private files
+#
+
+# ssh config
+if [[ -e "${HOME}/.ssh/config" ]]; then
+    rm -fv "${HOME}/.ssh/config"
+fi
+
+ln -sfv "${PWD}/ssh_config" "${HOME}/.ssh/config"
+
+# irssi config
+if [[ -e "${HOME}/.irssi/config" ]]; then
+    rm -fv "${HOME}/.irssi/config"
+fi
+
+ln -sfv "${PWD}/irssi_config" "${HOME}/.irssi/config"
