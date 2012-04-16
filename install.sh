d6af8ae640c032af2c15c2a86da150d4110fc510
Josh Enders <josh.enders@gmail.com>
2012-04-16 15:41:58 -0700

    really lazy handling of private files to be fixed later

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # This script creates symlinks in $HOME for certain files in this directory
 
+# FIXME: these should really be backed up, this will sting at some point
 printf "WARNING: This operation will remove existing files in $HOME.\n\n"
 
 while [[ -z "$response" ]]; do
@@ -17,18 +18,21 @@ while [[ -z "$response" ]]; do
     fi
 done
 
+# FIXME: This is not elegant but works for now
+
 #
 # Public files
 #
 
 dotfiles=(
     .bash_profile
-    .bash_profile.linux
-    .bash_profile.osx
+    .bash_profile.linux.aliases
+    .bash_profile.linux.environment
+    .bash_profile.osx.aliases
+    .bash_profile.osx.environment
     .gitconfig
     .htoprc
     .screenrc
-    .vim
     .vimrc
     .my.cnf
 ) 
@@ -43,12 +47,21 @@ done
 # Private files
 #
 
+dotfiles=(
+    .bash_profile.linux.aliases.fb
+)
+
 echo -e "\nPrivate files:"
+
+for file in ${dotfiles[@]}; do
+    rm -rf "${HOME}/.${file}"
+    ln -sfv "${PWD}/${file}" "${HOME}/${file}" # create symlinks for each file pointing here
+done
 
 # ssh config
 rm -f "${HOME}/.ssh/config"
-ln -sfv "${PWD}/ssh_config" "${HOME}/.ssh/config"
+ln -sfv "${PWD}/ssh_config.fb" "${HOME}/.ssh/config"
 
 # irssi config
 rm -f "${HOME}/.irssi/config"
-ln -sfv "${PWD}/irssi_config" "${HOME}/.irssi/config"
+ln -sfv "${PWD}/irssi_config.fb" "${HOME}/.irssi/config"
