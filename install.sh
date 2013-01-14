8e42790eff187ef799aba1fe96edd3b3767ba8fa
Josh Enders <josh.enders@gmail.com>
2013-01-14 02:36:35 -0800

    changes color and formatting

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # This script creates symlinks in $HOME for certain files in this directory
+red='\e[0;31m'
+bold='\e[1m'
+end='\e[m'
 
-# FIXME: these should really be backed up, this will sting at some point
-printf "WARNING: This operation will remove existing files in $HOME.\n\n"
+printf "${red}WARNING:${end} This operation will remove existing files in ${bold}${HOME}${end}.\n\n"
 
 while [[ -z "$response" ]]; do
     read -p "Are you ready to install? [y/n] " response
@@ -46,5 +48,6 @@ esac
 echo "Installing..."
 for file in ${dotfiles[@]}; do
     rm -rf "${HOME}/${file}"
+    printf "  "
     ln -sfv "${PWD}/${file}" "${HOME}/${file}" # create symlinks for each file pointing here
 done
